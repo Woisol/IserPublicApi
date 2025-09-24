e2630effe8ce9d6e720fc769d4c3bd020de7c4d9
Woisol-G <Woisol-G@outlook.com>
2025-09-24 10:28:30 +0800

    「 :bug: 修复 / fix, :rocket: 构建 / build 」: action 添加移除旧产物，添加 .env 存在判断逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { loadEnvFile } from 'process';
+import { readFileSync } from 'fs';
 
-loadEnvFile('.env');
+if (readFileSync('.env', 'utf-8')) {
+  loadEnvFile('.env');
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.SERVER_PORT || 3000;
